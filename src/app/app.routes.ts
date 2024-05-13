aa601652accd6714a85065f0f8ffa815a1d61dcc
Matthew C. Lind <overboardmediagroup@mail.com>
2024-05-13 14:59:51 -0400

    added new files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
+export const routes: Routes = [
+      {
+        path: '', title: 'Home Page', component: HomeComponent,
+      },
+      {
+        path: 'about', title: 'About Page', component: AboutComponent,
+      },
+];
